3b9a0d9553efa99a715aec4895d792a8de71336b
Joao <13579770+devOneto@users.noreply.github.com>
2024-10-26 16:20:18 -0300

    Hello Graphics!

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,29 @@
 
 int main() {
     
-    std::cout << "Hello World!" << std::endl;
+    // Image
 
-    return 0;
+    int img_width = 256;
+    int img_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << img_width << ' ' << img_height << '\n' << "255\n";
+
+    for( int j = 0; j < img_height; j++ ) {
+        for ( int i = 0; i < img_width; i++ ) {
+            
+            auto r = double(i) / ( img_width-1 );
+            auto g = double(j) / ( img_height-1 );
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
+
+        }
+    }
+
 }
